33edcbc5641c3c9ee12ac861502d90ca799560f2
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-03-22 05:07:31 +0100

    created 2 files for tasks 6 and 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,15 +6,12 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
-BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
+BEGIN 
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (SELECT AVG(score)
+                        FROM corrections
+                        WHERE corrections.user_id = user_id)
     WHERE id = user_id;
-END;
 //
 
 DELIMITER ;
